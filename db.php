d952ca91d599274fb94fda4dcfc8be6c20115ea9
andradex02 <qndrade2007@gmail.com>
2024-11-21 18:41:12 -0500

    db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "b3xoi93axiyfc4ylpz1k-mysql.services.clever-cloud.com"; // Cambia esto si tu servidor no está en localhost
+$servername = "b3xoi93axiyfc4ylpz1k-mysql.services.clever-cloud.com:3306"; // Cambia esto si tu servidor no está en localhost
 $username = "uhlnuouvu8r9rv6i";        // Usuario de MySQL (por defecto es "root")
 $password = "V0JTbJdTHuhT9yrx73JY";            // Contraseña de MySQL (por defecto es "")
-$dbname = "datos_sensor";      // Nombre de la base de datos
+$dbname = "b3xoi93axiyfc4ylpz1k";      // Nombre de la base de datos
 
 // Crear la conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
