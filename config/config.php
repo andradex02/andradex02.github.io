dd84390e26c51ea5196043149cc00ed17d668c57
andradex02 <qndrade2007@gmail.com>
2024-11-21 16:17:48 -0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,13 +1,25 @@
 <?php
 
+
+
 /* Database connection values */
+
 define("DB_HOST", "localhost");
-define("DB", "clima");
+
+define("DB", "sensores");
+
 define("DB_USER", "root");
+
 define("DB_PASS", "");
 
+
+
 /* Default options */
-define("DEFAULT_CONTROLLER", "datos_clima");
+
+define("DEFAULT_CONTROLLER", "sensor");
+
 define("DEFAULT_ACTION", "list");
+
+
 
 ?>
